4a9f5f649156d35d942d241e389dafdeceee28df
MoutonDemocrate <85873312+MoutonDemocrate@users.noreply.github.com>
2023-06-28 22:48:15 +0200

    Added HTTPrequest test
    it works

---
+++ TestDownload.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func _on_TestDL_button_pressed():
+	$HTTPRequest.request_completed.connect(_on_request_completed)
+	$HTTPRequest.set_download_file("FabricAPI.zip")
+	$HTTPRequest.request("https://cdn.modrinth.com/data/P7dR8mSH/versions/0.46.1%2B1.17/fabric-api-0.46.1%2B1.17.jar")
+
+func _on_request_completed(result, response_code, headers, body):
+	$Button.text = "It worked !"
+	pass
